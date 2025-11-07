14a46759d0b49856899530bacee80793ebb0b42a
aapokoiv <koivula.aapo@gmail.com>
2025-11-07 22:08:16 +0200

    Fixed the tests

--- src/varasto.py
+++ src/varasto.py
@@ -36,4 +36,4 @@ class Varasto:
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}" #eeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
